5b074134a7dd94ea34290fb967a9936bc16f96f6
Yusuf Öndü <yusufondu@Yusufs-MacBook-Pro.local>
2023-06-13 14:12:09 +0300

    tic tac toe game was created

---
+++ src/components/Square.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
